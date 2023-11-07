218cd76d4279dc6193d64e7514bd9ceaef6e284b
crismigo <cristinamigo01@gmail.com>
2023-11-07 18:11:25 +0100

    gamemanagaer

---
+++ src/GameManager.cpp
@@ -0,0 +1,78 @@
+#include "GameManager.h"
+
+
+void GameManager::init()
+{
+	score = 0;
+	lives = 3;
+	time = 5;
+	level = 1;
+	coins = 0;
+	startTime = std::chrono::high_resolution_clock::now();
+}
+
+void GameManager::update(int deltaTime)
+{
+	// Each second , time is reduced by 1
+	auto currentTime = std::chrono::high_resolution_clock::now();
+	auto timePassed = std::chrono::duration_cast<std::chrono::seconds>(currentTime - startTime).count();
+	if (timePassed >= 1) {
+		time--;
+		startTime = std::chrono::high_resolution_clock::now();
+	}
+
+}
+
+void GameManager::render()
+{
+}
+
+void GameManager::setScore(int score)
+{
+	this->score = score;
+}
+
+void GameManager::substractLive(int lives)
+{
+	--lives;
+}
+
+void GameManager::setTime(int time)
+{
+	this->time = time;
+}
+
+void GameManager::setLevel(int level)
+{
+	this->level = level;
+}
+
+void GameManager::setCoins(int coins)
+{
+	this->coins = coins;
+}
+
+int GameManager::getScore()
+{
+	return score;
+}
+
+int GameManager::getLives()
+{
+	return lives;
+}
+
+int GameManager::getTime()
+{
+	return time;
+}
+
+int GameManager::getLevel()
+{
+	return level;
+}
+
+int GameManager::getCoins()
+{
+	return coins;
+}
---
+++ src/GameManager.h
@@ -0,0 +1,54 @@
+#ifndef _GAMEMANAGER_INCLUDE
+#define _GAMEMANAGER_INCLUDE
+
+#include <glm/glm.hpp>
+#include "ShaderProgram.h"
+#include "json/json.h"
+#include <memory>
+#include <chrono>
+
+
+// GameManager contains all the entities of our game.
+// It is responsible for updating and render them.
+
+
+class GameManager
+{
+
+public:
+	static GameManager& instance()
+	{
+		static GameManager GM;
+
+		return GM;
+	}
+
+	void init();
+	void update(int deltaTime);
+	void render();
+
+	void setScore(int score);
+	void substractLive(int lives);
+	void setTime(int time);
+	void setLevel(int level);
+	void setCoins(int coins);
+
+	int getScore();
+	int getLives();
+	int getTime();
+	int getLevel();
+	int getCoins();
+
+
+
+
+private:
+	int score, lives, time, level, coins;
+	std::chrono::time_point<std::chrono::high_resolution_clock>  startTime;
+	
+
+};
+
+
+#endif // _GAMEMANAGER_INCLUDE
+
